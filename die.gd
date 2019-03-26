d625d602c01b2f476cd9126e65cb2252dd3260c9
Georg Tennigkeit <georg.tennigkeit@student.hpi.de>
2019-03-26 20:15:36 +0100

    added rollable die

---
+++ die.gd
@@ -0,0 +1,39 @@
+extends RigidBody
+
+var maxTorque = 200
+var maxForce = 200
+
+var stillRolling = true
+
+func _ready():
+	add_torque(Vector3(randi()%maxTorque - maxTorque/2, randi()%maxTorque - maxTorque/2, randi()%maxTorque - maxTorque/2))
+	add_central_force( Vector3(randi()%maxForce - maxForce/2, randi()%maxForce - maxForce/2, randi()%maxForce - maxForce/2))
+
+
+func _process(delta):
+	if (stillRolling == true):
+		if (linear_velocity.length() <= 0.1 && angular_velocity.length() <= 0.1):
+			stillRolling = false
+			print_debug(currentlyUp())
+
+#x = 6
+#y = 2
+#z = 4
+
+func currentlyUp():
+	var basis = transform.basis
+	if (basis.x.angle_to(Vector3.UP) < 0.5):
+		return 6
+	elif (basis.x.angle_to(Vector3.UP) > 2.5):
+		return 1
+	elif (basis.y.angle_to(Vector3.UP) < 0.5):
+		return 2
+	elif (basis.y.angle_to(Vector3.UP) > 2.5):
+		return 5
+	elif (basis.z.angle_to(Vector3.UP) < 0.5):
+		return 4
+	elif (basis.z.angle_to(Vector3.UP) > 2.5):
+		return 3
+	else:
+		print_debug("Something went wrong!")
+		return 0
